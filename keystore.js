45e0d21ebcdb4cbe724556695e4afc765fc86728
Yorick <yorickvanpelt@gmail.com>
2011-09-18 18:09:38 +0200

    Avoid second reduce on KeyStore.prototype._get

--- keystore.js
+++ keystore.js
@@ -4,10 +4,9 @@ Object.defineProperties(KeyStore.prototype,
   { _get: { value: function(x) {
         var terms = x.split('.')
         if (terms.length == 1) return this[terms[0]]
-        terms.slice(0, -1).reduce(function(prev, cur) {
+        return terms.slice(0, -1).reduce(function(prev, cur) {
             if (!(cur in prev)) prev[cur] = KeyStore(Object.getPrototypeOf(this))
-            return prev[cur] }, this)
-        return terms.reduce(function(prev, cur) { return prev[cur] })} }
+            return prev[cur] }, this)[terms[terms.length - 1]]} }
   , _set: { value: function(x, y) {
         var terms = x.split('.')
         if (terms.length == 1) return this[terms[0]] = y

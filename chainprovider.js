98c09211ba446e8257dfae01d6def4803eff9dd6
Yorick <yorickvanpelt@gmail.com>
2011-09-18 12:00:04 +0200

    Add arguments object to tap

--- chainprovider.js
+++ chainprovider.js
@@ -17,9 +17,9 @@ ChainProvider.prototype =
                 q.shift()
                 return this._provide.apply(this, arguments) }
             q = q[0] }
-        if (q[0].condition.apply(this, arguments)) {
+        if (q[0].condition.apply(this, q[0].arguments.concat(Array.prototype.slice.call(arguments)))) {
             return q.shift() }}
-    , tap: function(f, nonest, condition) { // pass nonest to true to make a lightweight tap
+    , tap: function(f, nonest, condition, args) { // pass nonest to true to make a lightweight tap
         if (!condition) condition = Ktrue
         var a
         if (!nonest) a = []
@@ -31,7 +31,7 @@ ChainProvider.prototype =
                     f.apply(this, arguments)
                     this._queue = t
                     if(a.length == 0 && t[0] == a) t.shift() } // assert(t[0] == a)?
-          , condition: condition })
+          , condition: condition, arguments: args || [] })
         if (!nonest) this._queue.push(a)
         return this }
      , loop: function(f) { // time to do evil :-)
